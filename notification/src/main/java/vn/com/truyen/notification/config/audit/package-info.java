3a85e6bf10c489a35bce47c46f56bafc2eaa846e
Toan <tn.toannguyen2811@gmail.com>
2020-06-18 11:18:04 +0700

    Initial version of notification generated by JHipster-6.9.1

---
+++ notification/src/main/java/vn/com/truyen/notification/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package vn.com.truyen.notification.config.audit;
---
+++ notification/src/main/java/vn/com/truyen/notification/domain/enumeration/NotificationType.java
@@ -0,0 +1,8 @@
+package vn.com.truyen.notification.domain.enumeration;
+
+/**
+ * The NotificationType enumeration.
+ */
+public enum NotificationType {
+    EMAIL, SMS, PARCEL
+}
